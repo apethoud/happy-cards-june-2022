27e0e3e101f1356a938581e7956bc3974752fa06
Andrew Pethoud <andrewpethoud@gmail.com>
2022-06-29 21:10:50 -0700

    add highlight cards and an example movie card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
+import HighlightCard from "./HighlightCard";
+import MovieCard from "./MovieCard";
+
+// Feel free to transform this into an array of movies.
+const movieData = {
+  Title: "The Avengers",
+  Year: "2012",
+  Rated: "PG-13",
+};
+
 export default function Home() {
   return (
     <div>
-      <h1>Send them a smile with HappyCards!</h1>
-      <h2>Choose from thousands of things...</h2>
+      <h1 className="HeaderText">Send them a smile with HappyCards!</h1>
+      <h2 style={{ fontStyle: "italic", marginTop: 40 }}>
+        Choose from thousands of things...
+      </h2>
+      <div style={{ display: "flex" }}>
+        <HighlightCard header="Select a Holiday" />
+        <HighlightCard header="Pick a Design" />
+        <HighlightCard header="Let Us Deliver It" />
+      </div>
+      {/* Below is an example of how you might start building a movie card. */}
+      <MovieCard title={movieData.Title} />
     </div>
   );
 }
---
+++ src/components/MovieCard.js
@@ -0,0 +1,7 @@
+export default function MovieCard(props) {
+  return (
+    <div>
+      <h1>Title: {props.title}</h1>
+    </div>
+  );
+}
