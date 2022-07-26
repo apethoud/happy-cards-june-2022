3566af21ff547bf71c57a309f0fe08627de09256
Andrew Pethoud <andrewpethoud@gmail.com>
2022-07-25 21:24:06 -0700

    created dark theme and toggle button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import HeaderFooter from "./components/HeaderFooter";
 import CardBuilder from "./components/CardBuilder";
+import Home from "./components/Home";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <HeaderFooter>
-      <CardBuilder />
-    </HeaderFooter>
+    <ThemeProvider>
+      <HeaderFooter>
+        <Home />
+        <CardBuilder />
+      </HeaderFooter>
+    </ThemeProvider>
   );
 }
 
